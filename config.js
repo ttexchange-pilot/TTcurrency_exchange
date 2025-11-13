41b0d9c018f5f21faf4da0461e679482bb26e138
ttexchange-pilot.github.io (Public) <rattiyapornsamrong@gmail.com>
2025-11-13 14:19:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,217 @@
+// Multi-Company + Branch config
+window.TTConfig = {
+  mode: "central",
+  apiBase: "http://localhost:3000/api",
+
+  companies: {
+  "HEADQUARTERS": {
+    "logo": "assets/img/logo_lockup.png",
+    "branches": {
+      "HM": {
+        "company": "TUNTHAVORN MARKETING LIMITED PARTNERSHIP HEADQUARTERS — HM",
+        "address": "Pattaya, Chonburi",
+        "Licence": "MC125510027",
+        "TaxId": "TAX ID : 0-20355-1001-73-0",
+        "contact": "Tel: 038-427-717"
+      }
+    }
+  },
+  "SOI BUAKHAO": {
+    "logo": "assets/img/logo_lockup.png",
+    "branches": {
+      "FM": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — FM",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      },
+      "RY": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — RY",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      },
+      "CP": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — CP",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      },
+      "B1": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — B1",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      },
+      "B3": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — B3",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      },
+      "B4": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — B4",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      },
+      "B5": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — B5",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      },
+      "B6": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — B6",
+        "address": "Soi Buakhao, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      }
+    }
+  },
+  "VILLA MARKET": {
+    "logo": "assets/img/logo_lockup.png",
+    "branches": {
+      "LW": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — LW",
+        "address": "Sukumvit, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      }
+    }
+  },
+  "SOUTH PATTAYA": {
+    "logo": "assets/img/logo_lockup.png",
+    "branches": {
+      "IT": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — IT",
+        "address": "South Pattaya, Chonburi",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      },
+      "KK": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — KK",
+        "address": "South Pattaya, Chonburi",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      },
+      "WK": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — WK",
+        "address": "South Pattaya, Chonburi",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 038-427-717"
+      }
+    }
+  },
+  "JOMTIEN": {
+    "logo": "assets/img/logo_lockup.png",
+    "branches": {
+      "JT1": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT1",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "JT2": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT2",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "JT3": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT3",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "JT4": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT4",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "JT5": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT5",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "JT6": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT6",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "JT7": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — JT7",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "PP1": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — PP1",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "PP2": {
+        "company": "T.T. CURRENCY EXCHANGE COMPANY LIMITED — PP2",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125540101",
+        "TaxId": "TAX ID 0-2055-54023-45-1",
+        "contact": "Tel: 033-129-900"
+      },
+      "CB": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — CB",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      },
+      "MB": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — MB",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      },
+      "M": {
+        "company": "T.T. TRADING CORPORATION COMPANY LIMITED — M",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125610028",
+        "TaxId": "TAX ID 0-2055-61011-18-7",
+        "contact": "Tel: 038-427-717"
+      },
+      "FL": {
+        "company": "TAWEE TOONTHAWON COMPANY LIMITED — FL",
+        "address": "Jomtien, Pattaya",
+        "Licence": "MC125490074",
+        "TaxId": "TAX ID 0-20554-9003-72-2",
+        "contact": "Tel: 038-427-717"
+      }
+    }
+  }
+},
+
+  pins: { staff: "1111", manager: "5599", admin: "8888" }
+};
